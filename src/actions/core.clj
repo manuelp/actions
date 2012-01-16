671baa6a18a6d5f778f835ac088f6945b3f9c3a4
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-01-16 23:41:14 +0100

    [WIP] First formatting functions

--- src/actions/core.clj
+++ src/actions/core.clj
@@ -5,3 +5,25 @@
 
 (defn read-data [file-name]
   (read-string (slurp file-name)))
+
+;(def actions (ref []))
+
+(defn new-action [description tags]
+  (dosync (alter actions conj {:description description :tags tags :done false})))
+
+(defn save-actions []
+  (write-data @actions "actions.data"))
+
+(defn load-actions []
+  (dosync (ref-set actions (read-data "actions.data"))))
+
+(defn format-tags [tags]
+  (loop [res (name (first tags)) t (rest tags)]
+    (if (empty? (rest t))
+      res
+      (recur (str res "," (name (first t))) (rest t)))))
+
+(defn format-action [action]
+  (if (:done action)
+    (str "[x] " (:description action) " (" (format-tags action ")"))
+    (str "[ ] " (:description action) " (" (format-tags action ")"))))
