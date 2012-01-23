d3a378a766949a6f7ca9d5a9cad3558f3268613a
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-01-23 23:54:23 +0100

    No shared state, but functions less usable in the REPL

--- src/actions/core.clj
+++ src/actions/core.clj
@@ -10,23 +10,11 @@
       (read-string data)
       [])))
 
-(def actions (ref []))
-
-(defn next-id []
-  (if (empty? @actions)
-    1
-    (inc (apply max (map #(:id %) @actions)))))
-
-(defn new-action [description tags]
-  (dosync (alter actions conj {:id (next-id) :description description :tags tags :done false})))
-
-(defn save-actions []
-  (write-data @actions "actions.data"))
+(defn save-actions [actions]
+  (write-data actions "actions.data"))
 
 (defn load-actions []
-  (dosync (ref-set actions (read-data "actions.data"))))
-
-(load-actions)
+  (read-data "actions.data"))
 
 (defn format-tags [tags]
   (join \, (map (fn [sym]
@@ -38,15 +26,22 @@
     (str (action :id)  ": [x] " (action :description) " (" (format-tags (action :tags)) ")")
     (str (action :id) ": [ ] " (action :description) " (" (format-tags (action :tags)) ")")))
 
-(defn print-actions []
-  (println (join \newline (map format-action (sort-by #(:description %) @actions)))))
+(defn print-actions [actions]
+  (println (join \newline (map format-action (sort-by #(:description %) actions)))))
 
-(defn mark-done [id]
-  (dosync (ref-set actions (map (fn [a]
-                                  (if (= (:id a) 1)
-                                    (assoc a :done true)
-                                    a))
-                                @actions))))
+(defn next-id [actions]
+  (if (empty? actions)
+    1
+    (inc (apply max (map #(:id %) actions)))))
 
-;; Test
-(print-actions)
+(defn add-action [description tags actions]
+  (conj actions {:id (next-id actions) :description description :tags tags :done false}))
+
+(defn mark-done [id actions]
+  (vec (map (fn [a]
+         (if (= (:id a) id)
+           (assoc a :done true)
+           a))
+       actions)))
+
+(print-actions (load-actions))
