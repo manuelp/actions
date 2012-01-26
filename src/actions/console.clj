b4e64fb1143b4972fc726dcc9ae4a26fe4fae496
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-01-26 20:51:03 +0100

    Actions correctly sorted by priority and description

--- src/actions/console.clj
+++ src/actions/console.clj
@@ -9,9 +9,18 @@
 (defn format-action [action]
   (str (action :id) " (" (action :priority) ") " (action :description) " (" (format-tags (action :tags)) ")"))
 
+(defn take-with-priority [actions]
+  (filter #(not (nil? (:priority %))) actions))
+
+(defn take-without-priority [actions]
+  (filter #(nil? (:priority %)) actions))
+
+(defn sort-actions [actions]
+  (let [with-p (take-with-priority actions)
+        without-p (take-without-priority actions)]
+    (concat (sort-by #(vec (map % [:priority :description])) with-p)
+            (sort-by :description without-p))))
+
 (defn print-actions [actions]
   (println (str/join \newline
-                 (map format-action
-                      (sort-by #(:description %)
-                               (sort-by #(:priority %)
-                                        (filter #(not (% :done)) actions)))))))
+                 (map format-action (sort-actions (filter #(not (% :done)) actions))))))
