1e3459ae9b9cadafe434ebce25276c8bec484c2c
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-01-31 21:54:27 +0100

    Removed joda-time from project dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject actions "1.0.0"
   :description "A simple actions manager with the todo.txt plain text format."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [joda-time/joda-time "2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
