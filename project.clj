d9670398f75221f71e8106e53b9956d97cc2b9dc
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-16 21:59:03 +0100

    Version number bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject actions "1.0.0"
+(defproject actions "1.0.1"
   :description "A simple actions manager with the todo.txt plain text format."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :main actions.console)
