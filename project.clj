a77e9070259515ed141a51b1cd676c540abf2fd0
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-05-21 22:09:59 +0200

    Updated to clojure 1.4.0, Leiningen 2 and lein-swank

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject actions "1.0.2"
+(defproject actions "1.0.3-SNAPTSHOT"
   :description "A simple actions manager with the todo.txt plain text format."
+  :url "https://github.com/manuelp/actions"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [bronsa/colorize "0.1.2"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]]
